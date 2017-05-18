c0da17809c6f8c6085cb2771d1f97259612952a0
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2017-05-18 10:40:28 +0200

    update commands

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+import commands
+
+commands.getstatusoutput('otree resetdb')
 
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from otree.management.cli import execute_from_command_line
-    execute_from_command_line(sys.argv, script_file="script.sh")
     execute_from_command_line(sys.argv, script_file=__file__)
